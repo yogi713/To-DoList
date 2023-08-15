5e62737530307da850c46ac33f8603a12dcacbfc
yogi713 <yogesh71352@gmail.com>
2023-08-15 21:21:06 +0530

    yes

--- todo.js
+++ todo.js
@@ -8,7 +8,11 @@ const listArray =
     dueDate: '2022-12-12'
 }];
 
-generateHTML();
+function darkBody(){
+    document.querySelector('body').style.background= "linear-gradient(135deg, #34556b, #2e316b)";
+}
+
+// generateHTML();
 function generateHTML(){
     let HTMLcode = '';
     for(let i=0;i<listArray.length;i++){
@@ -31,6 +35,10 @@ function generateHTML(){
 
 function addToArray(){
     const name = document.querySelector(".text").value;
+    if(name === ''){
+        document.querySelector(".text").placeholder = 'Type here to add';
+        return;
+    }
     const dueDate = document.querySelector(".date").value;
     // const obj = {name: name, dueDate: dueDate};
     const obj = {name, dueDate}; // ShortHand Syntax
